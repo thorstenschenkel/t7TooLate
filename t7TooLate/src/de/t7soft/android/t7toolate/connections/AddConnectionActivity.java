4a9da9d988075ee3d7848b97919ef3527799d382
Thorsten Schenkel (t7soft.de) <Thorsten Schenkel (t7soft.de)@compeople-186.compeople.local>
2016-07-07 14:12:21 +0200

    copyright headers added

--- t7TooLate/src/de/t7soft/android/t7toolate/connections/AddConnectionActivity.java
+++ t7TooLate/src/de/t7soft/android/t7toolate/connections/AddConnectionActivity.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2016 Thorsten Schenkel (t7soft.de)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * @author Thorsten Schenkel (t7soft.de)
+ * 
+ */
 package de.t7soft.android.t7toolate.connections;
 
 import android.os.Bundle;
@@ -37,9 +59,9 @@ public class AddConnectionActivity extends EditConnectionActivity {
 
 	@Override
 	protected boolean save() {
-		long rowId = dbAdapter.insertConnection(connection);
+		final long rowId = dbAdapter.insertConnection(connection);
 		if (rowId == -1) {
-			String errorMsg = getString(R.string.connection_error_insert);
+			final String errorMsg = getString(R.string.connection_error_insert);
 			showErrorMsg(errorMsg);
 			return false;
 		}
